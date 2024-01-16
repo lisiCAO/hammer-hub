a2d3476e99b507ca748ded02fd382cfba782bd1c
CAO LISI <137085653+lisiCAO@users.noreply.github.com>
2024-01-16 19:27:16 +0000

    create crud

--- backend/models/Auction.js
+++ backend/models/Auction.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = require('./database');
+const sequelize = require('../database');
 
 const Auction = sequelize.define('auction', {
     /*
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,70 @@
 const express = require('express');
-const sequelize = require('./database');
-
+const Auction = require('./models/Auction');
 const app = express();
+app.use(express.json());
+
+/*
+GET /api/auctions
+GET /api/auction/123 (if needed)
+POST /api/auctions
+PATCH /api/auctions/123
+*/
+app.post('/api/auctions', async (req, res) => {
+    try{
+        const newAuction = await Auction.create(req.body);
+        res.status(201).json(newAuction);
+    } catch (error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.get('/api/auctions', async (req, res) => {
+    try{
+        const auctions = await Auction.findAll();
+        res.json(auctions);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/api/auctions/:id', async (req, res) => {
+    try{
+        const auction = await Auction.findByPk(req.params.id);
+        res.json(auction);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.patch('/api/auctions/:id', async (req, res) => {
+    try{
+        const auction = await Auction.findByPk(req.params.id);
+        auction.update(req.body);
+        res.json(auction);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.put('/api/auctions/:id', async (req, res) => {
+    try{
+        const auction = await Auction.findByPk(req.params.id);
+        auction.update(req.body);
+        res.json(auction);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.delete('/api/auctions/:id', async (req, res) => {
+    try{
+        const auction = await Auction.findByPk(req.params.id);
+        auction.destroy();
+        res.json(auction);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
 const port = 3000;
-
-sequelize.authenticate()
-.then(() => console.log('Connection has been established successfully.'))
-.catch((error) => console.error('Unable to connect to the database:', error));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`) );
