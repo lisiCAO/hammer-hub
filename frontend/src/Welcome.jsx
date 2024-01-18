6ecf2f72bdb24494d869a273578d88f44186f20d
lisiCAO <caolisi@hotmail.com>
2024-01-18 10:52:13 -0500

    update classname

--- frontend/src/Welcome.jsx
+++ frontend/src/Welcome.jsx
@@ -6,8 +6,8 @@ const Welcome = () => {
         <div className="welcome">
             <h1 className="welcome__title">Welcome to Auctions!</h1>
             <p className="welcome__description">Auctions is a simple web application that allows users to create auctions and bid on them.</p>
-            <Link to="/auctions"> View Auctions </Link>
-            <Link to="/add-auction"> Add Auction </Link>
+            <Link to="/auctions" style={{ display: 'block', margin: '10px 0' }}> View Auctions </Link>
+            <Link to="/add-auction" style={{ display: 'block', margin: '10px 0' }}> Add Auction </Link>
         </div>
     );
 }
