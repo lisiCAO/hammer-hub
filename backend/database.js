ec62e1d270216e0bddca98ce6f541d152becddb6
lisiCAO <caolisi@hotmail.com>
2024-01-18 12:27:41 -0500

    delete

--- backend/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('hammerdb', 'user', 'password', {
-    host: 'db',
-    dialect: 'mysql',
-});
-
-module.exports = sequelize;
